9da79538110cc75d7fc57fa4bc3101af852f7c00
jek1m <jieunkim7912@gmail.com>
2025-03-14 10:06:15 +0900

    26번문제업로드

--- onelist.dart
+++ onelist.dart
@@ -1,14 +1,15 @@
 // 2025.03.14 아침에 코딩  advanc 반 26번 문제 정렬된 배열에서 중복 제거
 // 작성자 김지은
+
 void main() {
   List<int> nums = [0,0,1,1,2,2,3,3,3];
-  int j = 0;
-  int result = nums.length;
+
+  int j = 1;
   for (var i = 1 ; i < nums.length ; i++) {
-    if (nums[i] == nums[i-1]) {
+    if (nums[i] != nums[j-1]) {
+      nums[j] = nums[i];
       j += 1;
     }
   }
-  result -= j;
-  print('유효한 길이는 $result');
-}
+  print('유효한 길이는 $j');
+} 
