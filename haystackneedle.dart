07f34a96dd79dc50efc8d7163dfefb5c757b3cae
jek1m <jieunkim7912@gmail.com>
2025-03-18 09:51:06 +0900

    28번문제업로드

---
+++ haystackneedle.dart
@@ -0,0 +1,9 @@
+// 2025.03.17 아침에 코딩  advanc 반 28번 문제 문자열에서 첫 번째로 나타나는 인덱스 찾기
+// 작성자 김지은
+void main() {
+  String haystack = "leetcode";
+  String needle = "leeto";
+  int i = haystack.indexOf(needle);
+  print(i);
+}
+
