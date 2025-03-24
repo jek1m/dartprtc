ee84abd9d26033725abe1285251ea65d9cfac720
jek1m <jieunkim7912@gmail.com>
2025-03-24 11:21:04 +0900

    121번문제업로드

---
+++ solution.dart
@@ -0,0 +1,19 @@
+// 2025.03.24 아침에 코딩  advanc 반 121번 문제 주식 매수 및 매도의 최적 시기
+// 작성자 김지은
+
+class Solution {
+  int maxProfit(List<int> prices) {
+    int min = prices[0];
+    int maxPrice = 0;
+    int result = 0;
+    for(var i  = 0; i < prices.length; i++) {
+        if(min > prices[i]) {
+            min = prices[i];
+        }
+        if(prices[i]-min > maxPrice){
+            maxPrice = prices[i]-min;
+        }
+    }
+    return maxPrice;
+  }
+}
