7f1b8b30574a4ff81ffd332ad571d62db15bdd20
jek1m <jieunkim7912@gmail.com>
2025-03-26 09:46:41 +0900

    26번문제업로드

---
+++ singlenumber.dart
@@ -0,0 +1,19 @@
+// 2025.03.14 아침에 코딩  advanc 반 26번 문제 정렬된 배열에서 중복 제거
+// 작성자 김지은
+
+class Solution {
+  int singleNumber(List<int> nums) {
+    int num = 0;
+    for (int i in nums ) {
+      num ^= i;
+    }
+    return num;
+  }
+}
+
+void main() {
+  List<int> nums = [4,1,2,1,2];
+  Solution solution = Solution();
+  int result = solution.singleNumber(nums);
+  print(result);
+}
